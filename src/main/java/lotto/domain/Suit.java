a8e4c6fc0b6cb4987acf5d6221dfe445e2bf3df4
gashe-soo <gashe7@gmail.com>
2021-08-22 22:53:02 +0900

    feat:make Card Instance Cache

---
+++ src/main/java/lotto/domain/Suit.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+public enum Suit {
+    HEART("하트"),
+    CLOVER("클로버"),
+    SPADE("스페이드"),
+    DIAMOND("다이아몬드");
+
+    private final String name;
+
+    Suit(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
